ae07e037ea5bb04614a3f47d13d3056b181bee24
Priscilla Reis <priscillareissouza@outlook.com>
2021-09-19 22:25:56 -0300

    atividade previa - ARI

---
+++ src/main/java/com/nomealuno/demoacmeap/repository/InstalacaoRepository.java
@@ -0,0 +1,17 @@
+package com.nomealuno.demoacmeap.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nomealuno.demoacmeap.domain.Cliente;
+import com.nomealuno.demoacmeap.domain.Instalacao;
+
+public interface InstalacaoRepository extends JpaRepository<Instalacao, Long> {
+
+	public Optional<Instalacao> findByCodigo(String codigo);
+	public List<Instalacao> findByCliente(Cliente cliente);
+	
+	
+}
